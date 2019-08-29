ae402e2a8adbbf207d941e11079b99be6fe2f694
xnzale <bdnzale@gmail.com>
2019-08-29 10:53:17 +0000

    first commit

---
+++ var/cache/dev/twig/0f/0f086151c8f78a59c4c13ac5aec488ef93a85a3cea2c97c41a4cd9f273dde418.php
@@ -0,0 +1,70 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Framework/FormTable/hidden_row.html.php */
+class __TwigTemplate_4708883b14b1ec075a0a7b3e21d70c3ae7f3df4b2f9f62b3c2b654eae93c857b extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Framework/FormTable/hidden_row.html.php"));
+
+        // line 1
+        echo "<tr style=\"display: none\">
+    <td colspan=\"2\">
+        <?php echo \$view['form']->widget(\$form); ?>
+    </td>
+</tr>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Framework/FormTable/hidden_row.html.php";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<tr style=\"display: none\">
+    <td colspan=\"2\">
+        <?php echo \$view['form']->widget(\$form); ?>
+    </td>
+</tr>
+", "@Framework/FormTable/hidden_row.html.php", "/home/nzale/transfert/vendor/symfony/framework-bundle/Resources/views/FormTable/hidden_row.html.php");
+    }
+}
